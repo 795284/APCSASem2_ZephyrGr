4f2f15ea936eea316db85d4d7cb868e8518ba994
ZephyrGr <42661953+ZephyrGr@users.noreply.github.com>
2019-01-18 10:34:43 -0800

    Add files via upload

---
+++ Swapfuction.java
@@ -0,0 +1,25 @@
+
+/**
+ *Zephyr granger
+ *Swap Fuction
+ *1/10/19
+ */
+public class Swapfuction
+{
+    private static int  x = 9;
+    private static int  y = 12;
+    public static  void swap(int a,int b){
+        System.out.println("a = " + a + ",  b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a + ",  b = " + b);
+    }
+    public static void main (){
+        int x = 9;
+        int y = 12;
+        swap(x,y);
+         System.out.println("x = " +x + ",  y = " + y);
+    }
+    }
+
